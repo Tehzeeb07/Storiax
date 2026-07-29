0e598c44de96ef6a841801ce63b4e4603bfd441b
zenbaayy <zenbaayy935@gmail.com>
2026-07-29 16:37:06 +0600

    updated Explore page colors to match theme

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
+import './styles/theme.css'
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { AuthProvider } from "./contexts/AuthContext";
+// src/main.jsx — sabse upar
+
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
